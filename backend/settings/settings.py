1cb8123fd4a0ff6df3e217291a557606b5c506fc
Suleyman Aliyev <36112126+suleyman735@users.noreply.github.com>
2024-03-13 10:09:09 +0100

    docker- file

--- backend/settings/settings.py
+++ backend/settings/settings.py
@@ -91,11 +91,11 @@ DATABASES = {
 
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ecomtest',
+        'NAME': 'ecommerce',
         'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '',
+        'PASSWORD': '551155',
+        'HOST': 'mysqlserver',
+        'PORT': '3306',
         'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"},
 
 
